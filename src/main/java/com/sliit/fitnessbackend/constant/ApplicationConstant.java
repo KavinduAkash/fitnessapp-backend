e6ff2ea1f6a3a4ebe673000ff9be8bfcffd064dc
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-03-30 17:32:40 +0530

    dev application constants

---
+++ src/main/java/com/sliit/fitnessbackend/constant/ApplicationConstant.java
@@ -0,0 +1,6 @@
+package com.sliit.fitnessbackend.constant;
+
+public class ApplicationConstant {
+    public static final String APPLICATION_ERROR_OCCURRED_MESSAGE = "Application Error Occurred";
+
+}
