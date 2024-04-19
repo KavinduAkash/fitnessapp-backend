9974dd139d3859d3f914cdb031faa5316b402cd9
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-04-19 21:28:18 +0530

    dev meal plan entity

---
+++ src/main/java/com/sliit/fitnessbackend/entity/MealPlan.java
@@ -0,0 +1,33 @@
+package com.sliit.fitnessbackend.entity;
+
+import com.sliit.fitnessbackend.enums.MealStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "meal_plan")
+public class MealPlan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String description;
+    private boolean isCurrent;
+    private MealStatus status;
+    private Date date;
+
+    public MealPlan() {
+    }
+
+    public MealPlan(Integer id, String title, String description, boolean isCurrent, MealStatus status, Date date) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.isCurrent = isCurrent;
+        this.status = status;
+        this.date = date;
+    }
+}
