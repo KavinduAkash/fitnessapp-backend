38ccbaad1cfb119b8799fa8eb5f9e2c40d5779d7
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-04-19 21:28:35 +0530

    dev meal entity

---
+++ src/main/java/com/sliit/fitnessbackend/entity/Meal.java
@@ -0,0 +1,59 @@
+package com.sliit.fitnessbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "meal")
+public class Meal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String mealName;
+    private String description;
+    @ManyToOne
+    @JoinColumn(name = "meal_plan", nullable = false)
+    private MealPlan mealPlan;
+
+    public Meal() {
+    }
+
+    public Meal(Integer id, String mealName, String description) {
+        this.id = id;
+        this.mealName = mealName;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMealName() {
+        return mealName;
+    }
+
+    public void setMealName(String mealName) {
+        this.mealName = mealName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public MealPlan getMealPlan() {
+        return mealPlan;
+    }
+
+    public void setMealPlan(MealPlan mealPlan) {
+        this.mealPlan = mealPlan;
+    }
+}
