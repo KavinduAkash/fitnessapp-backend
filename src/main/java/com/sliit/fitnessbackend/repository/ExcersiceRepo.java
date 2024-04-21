e3ec8b95e64798e0a16b320f52186ad469d9a9e4
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-04-21 08:42:21 +0530

    develop excersice repo

---
+++ src/main/java/com/sliit/fitnessbackend/repository/ExcersiceRepo.java
@@ -0,0 +1,7 @@
+package com.sliit.fitnessbackend.repository;
+
+import com.sliit.fitnessbackend.entity.Excersice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExcersiceRepo extends JpaRepository<Excersice, Integer> {
+}
