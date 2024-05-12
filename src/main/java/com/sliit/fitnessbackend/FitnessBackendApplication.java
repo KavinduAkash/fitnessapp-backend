5b637d551623e13c605812a7eeb4dcd72c9b1e72
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-05-12 11:02:33 +0530

    update starter

--- src/main/java/com/sliit/fitnessbackend/FitnessBackendApplication.java
+++ src/main/java/com/sliit/fitnessbackend/FitnessBackendApplication.java
@@ -3,12 +3,25 @@ package com.sliit.fitnessbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableAsync
+@EnableTransactionManagement
+@EnableJpaAuditing
 public class FitnessBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FitnessBackendApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
